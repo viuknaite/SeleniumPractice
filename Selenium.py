bcad5f027d5c54deedc75de05cc24a14d2c6602e
viuknaite <viuknaite@gmail.com>
2025-04-21 14:18:29 +0300

    fixtures

--- Selenium.py
+++ Selenium.py
@@ -3,27 +3,32 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-#chrome_options = Options()
-#chrome_options.add_argument("--disable-extensions")
-
-# Initialize Chrome WebDriver
-driver = webdriver.Chrome() #options=chrome_options
-
-driver.get("https://www.wikipedia.org/")
-
-# Implicit wait
-driver.implicitly_wait(10)
-driver.find_element(By.XPATH,"//a//*[contains(text(), 'English')]").click()
-driver.find_element(By.XPATH, "//div[@id='p-search']//a").click()
-driver.find_element(By.XPATH, "//input[@class='cdx-text-input__input']").send_keys("qa engineer")
+import pytest
 
 
-# Explicit wait for an element to appear
-element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located(By.XPATH, "//div[@class='vector-typeahead-search-container']//button").click()
-)
+@pytest.fixture(scope="class")
+def browser(request):
+    driver = webdriver.Chrome()
+    request.cls.browser = driver
+    yield
+    driver.quit()
 
-driver.implicitly_wait(10)
+@pytest.mark.usefixtures("browser")
+class TestBrowserExample:
+    def test_example(self):
+        self.browser.get("https://www.wikipedia.org/")
+        assert "Wikipedia" in self.browser.title
 
-driver.quit()
+        # Implicit wait
+        self.browser.implicitly_wait(10)
+        self.browser.find_element(By.XPATH, "//a//*[contains(text(), 'English')]").click()
+        self.browser.find_element(By.XPATH, "//div[@id='p-search']//a").click()
+        self.browser.find_element(By.XPATH, "//input[@class='cdx-text-input__input']").send_keys("qa engineer")
+
+        # Explicit wait for an element to appear
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//div//form[@id='searchform']//button"))
+        )
+        element.click()
+
+        self.browser.quit()
