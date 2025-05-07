59d8ee177e986a63b9b9cda2103db71eb0d03267
twlv12 <nkkr135@gmail.com>
2025-05-07 16:44:57 +0300

    Добавлены try/except и скриншоты при ошибках (задача #8)

--- pages/article_page.py
+++ pages/article_page.py
@@ -1,14 +1,30 @@
+import os
+import time
 from selenium.webdriver.common.by import By
+
 
 class ArticlePage:
     def __init__(self, browser):
         self.browser = browser
 
-    def page_contains_text(self, text):
-        # Проверка заголовка страницы
-        if text.lower() in self.browser.title.lower():
-            return True
+    def save_screenshot(self, name):
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        path = f"screenshots/{name}_{timestamp}.png"
+        os.makedirs("screenshots", exist_ok=True)
+        self.browser.save_screenshot(path)
+        print(f"[!] Скриншот сохранён: {path}")
 
-        # Проверка содержимого страницы
-        body_text = self.browser.find_element(By.TAG_NAME, "body").text
-        return text.lower() in body_text.lower()
+    def page_contains_text(self, text):
+        try:
+            if text.lower() in self.browser.title.lower():
+                self.save_screenshot("check_title_success")
+                return True
+            body_text = self.browser.find_element(By.TAG_NAME, "body").text
+            if text.lower() in body_text.lower():
+                self.save_screenshot("check_body_success")
+                return True
+            self.save_screenshot("text_not_found")
+            return False
+        except Exception as e:
+            self.save_screenshot("error_check_text")
+            raise e
--- pages/english_ver.py
+++ pages/english_ver.py
@@ -1,29 +1,45 @@
 import time
+import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException
-from utils.screenshots import save_screenshot
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException, WebDriverException
+
 
 class EnglishVersion:
     def __init__(self, browser):
         self.browser = browser
 
+    def save_screenshot(self, step_name):
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        filename = f"screenshots/{step_name}_{timestamp}.png"
+        os.makedirs("screenshots", exist_ok=True)
+        self.browser.save_screenshot(filename)
+        print(f"[!] Скриншот сохранён: {filename}")
+
     def click_search_icon(self):
-        self.browser.find_element(By.XPATH, "//div[@id='p-search']//a").click()
-        save_screenshot(self.browser, "click_search_icon")
+        try:
+            self.browser.find_element(By.XPATH, "//div[@id='p-search']//a").click()
+            self.save_screenshot("click_search_icon")
+        except Exception as e:
+            self.save_screenshot("error_click_search_icon")
+            raise e
 
     def enter_search_text(self, text):
-        search_input = WebDriverWait(self.browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, "(//input[@class='cdx-text-input__input'])[1]"))
-        )
-        search_input.send_keys(text)
-        self.browser.implicitly_wait(3)
-        save_screenshot(self.browser, "enter_search_text")
+        try:
+            search_input = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.XPATH, "(//input[@class='cdx-text-input__input'])[1]"))
+            )
+            search_input.send_keys(text)
+            self.browser.implicitly_wait(3)
+            self.save_screenshot("enter_search_text")
+        except Exception as e:
+            self.save_screenshot("error_enter_search_text")
+            raise e
 
     def click_search_button(self):
         wait = WebDriverWait(self.browser, 10)
-        for _ in range(3):
+        for i in range(3):
             try:
                 button = wait.until(
                     EC.presence_of_element_located((By.CSS_SELECTOR, "button.cdx-search-input__end-button"))
@@ -31,27 +47,33 @@ class EnglishVersion:
                 WebDriverWait(self.browser, 10).until(
                     EC.element_to_be_clickable((By.CSS_SELECTOR, "button.cdx-search-input__end-button"))
                 ).click()
-                save_screenshot(self.browser, "click_search_button")
+                self.save_screenshot("click_search_button")
                 return
-            except StaleElementReferenceException:
+            except (StaleElementReferenceException, TimeoutException):
                 time.sleep(1)
-        raise Exception("Не удалось кликнуть по кнопке поиска из-за stale element")
+        self.save_screenshot("error_click_search_button")
+        raise Exception("Не удалось кликнуть по кнопке поиска")
 
     def handle_cookies(self):
-        cookies = self.browser.get_cookies()
-        print("До добавления куки:", cookies)
+        try:
+            cookies = self.browser.get_cookies()
+            print("До добавления куки:", cookies)
 
-        self.browser.add_cookie({
-            "name": "test_cookie",
-            "value": "12345",
-            "domain": "wikipedia.org"
-        })
+            self.browser.add_cookie({
+                "name": "test_cookie",
+                "value": "12345",
+                "domain": "wikipedia.org"
+            })
 
-        cookies = self.browser.get_cookies()
-        print("После добавления куки:", cookies)
+            cookies = self.browser.get_cookies()
+            print("После добавления куки:", cookies)
+            self.save_screenshot("handle_cookies")
+        except WebDriverException as e:
+            self.save_screenshot("error_handle_cookies")
+            raise e
 
     def open_first_result_in_new_tab(self):
-        for _ in range(3):
+        for i in range(3):
             try:
                 first_link = WebDriverWait(self.browser, 10).until(
                     EC.presence_of_element_located((By.XPATH, "(//div[contains(@class, 'mw-body-content')]//a)[1]"))
@@ -59,9 +81,10 @@ class EnglishVersion:
                 href = first_link.get_attribute("href")
                 self.browser.execute_script(f"window.open('{href}', '_blank');")
                 self.browser.switch_to.window(self.browser.window_handles[1])
-                save_screenshot(self.browser, "open_first_result_tab")
                 self.handle_cookies()
+                self.save_screenshot("open_first_result_in_new_tab")
                 return
             except StaleElementReferenceException:
                 time.sleep(1)
-        raise Exception("Не удалось открыть первую ссылку из-за stale element")
+        self.save_screenshot("error_open_first_result")
+        raise Exception("Не удалось открыть первую ссылку")
