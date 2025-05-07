9ebb1754fa73694965c2b8217a92c7ca2d8f031d
twlv12 <nkkr135@gmail.com>
2025-05-07 16:19:10 +0300

    Экспорт Allure-отчётов+скриншот

--- pages/article_page.py
+++ pages/article_page.py
@@ -5,7 +5,10 @@ class ArticlePage:
         self.browser = browser
 
     def page_contains_text(self, text):
+        # Проверка заголовка страницы
         if text.lower() in self.browser.title.lower():
             return True
+
+        # Проверка содержимого страницы
         body_text = self.browser.find_element(By.TAG_NAME, "body").text
         return text.lower() in body_text.lower()
--- tests/wiki_test.py
+++ tests/wiki_test.py
@@ -1,8 +1,10 @@
 import pytest
+import allure
 from selenium import webdriver
 from pages.main_page import MainPage
 from pages.english_ver import EnglishVersion
-from pages.article_page import ArticlePage  # добавлен новый импорт
+from pages.article_page import ArticlePage
+
 
 @pytest.fixture(scope="class")
 def browser(request):
@@ -11,8 +13,12 @@ def browser(request):
     yield
     driver.quit()
 
+
 @pytest.mark.usefixtures("browser")
 class TestSearch:
+
+    @allure.title("Проверка поиска статьи по запросу 'qa engineer'")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_search_qa_engineer(self):
         main_page = MainPage(self.browser)
         main_page.open()
@@ -27,5 +33,5 @@ class TestSearch:
         eng_page.click_search_button()
         eng_page.open_first_result_in_new_tab()
 
-        article = ArticlePage(self.browser)
-        assert article.page_contains_text("Wikipedia")
+        article_page = ArticlePage(self.browser)
+        assert article_page.page_contains_text("Wikipedia")
